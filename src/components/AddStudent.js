e52dbce6880a9e0931f516e27fed42af96ebf3dc
Edwin <edoumacodu@gmail.com>
2023-07-01 09:21:30 +0300

    initial setup of axios

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -1,12 +1,21 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios'
 
 
 const AddStudent = () => {
+    const saveStudent=()=>{
+        axios.post('http://localhost:4000/students')
+        .then(res=>{
+            console.log(res)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
     return (
         <div className='col-md-6 offset-md-3 addStudent'>
             <h3 className='mytext'> Add New student</h3>
-            <Form>
+            <Form onSubmit={saveStudent}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Firstname:</Form.Label>
                     <Form.Control type="input" placeholder="Enter Firstname" />
