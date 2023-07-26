8cba8e0d71d9de2b2057a5df394cb6949b7d3491
Edwin <edoumacodu@gmail.com>
2023-07-26 15:23:41 +0300

    added code the consume login endpoint to login a user once entered their credentials

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,55 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
-const login = () => {
+const Login = () => {
+
+    const [email, setEmail]= useState('')
+    const [password, setPassword]= useState('')
+    const [error, setError] = useState('')
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+      };
+
+    const handlePassword =(e)=>{
+        setPassword(e.target.value);
+    }
+
+    const loginUser = async(e)=>{
+        e.preventDefault()
+        try {    
+        const response = await axios.post('http://localhost:4000/auth/login', {email, password} ) 
+        
+        if (response.status === 200) {
+    
+            const { accessToken, refreshToken } = response.data;
+            
+            // Save access_token in session storage
+            sessionStorage.setItem('access_token', accessToken);
+            sessionStorage.setItem('refresh_token', refreshToken);
+
+            // Redirect or perform any other action upon successful login
+            window.location.href = '/'; // Change the URL to your desired route
+          } else {
+            const errorResponse = await response.json();
+            setError(errorResponse.message);
+          }
+            
+        } catch (error) {
+            setError('An error occurred. Please try again.');
+        }
+        
+    }
     return ( 
         <div className='form-content'>
              <h4 align="center"> Login </h4>
-            <Form>
+            <Form onSubmit={loginUser}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" value ={email} required onChange={handleEmail} placeholder="Enter email" 
+                    name="email"/>
                     <Form.Text className="text-muted">
                     
                     </Form.Text>
@@ -16,14 +57,17 @@ const login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" value={password} required onChange={handlePassword} placeholder="Password" 
+                    name="password"/>
                 </Form.Group>
+                    {error && <p className="text-danger">{error}</p>}
                 <Button variant="primary" type="submit">
                     Login
                 </Button>
             </Form>
         </div>
+       
      );
 }
  
-export default login;
+export default Login;
